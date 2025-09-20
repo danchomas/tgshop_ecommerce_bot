7d38bdbd816f8ab464a70652cd4aafc23de0da3c
danchomas <danchomas@gmail.com>
2025-09-20 13:44:02 +0300

    db structure

---
+++ src/data/models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Category(Base):
+    __tablename__ = 'categories'
+
+    id = Column(Integer, primary_key=True)
+    key = Column(String, unique=True, nullable=False)
+    name = Column(String, nullable=False)
+    items = relationship("MenuItem", back_populates="category")
+
+class MenuItem(Base):
+    __tablename__ = 'menu_items'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(String)
+    price = Column(Float, nullable=False)
+    category_id = Column(Integer, ForeignKey('categories.id'))
+
+    category = relationship("Category", back_populates="items")
